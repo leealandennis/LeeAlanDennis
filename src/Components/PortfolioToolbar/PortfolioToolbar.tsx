4a832392e72fa434da00321b63700af5176dff78
Lee Dennis <leealandennis@gmail.com>
2022-01-25 22:41:23 -0500

    feature/LD-1: app toolbar #LD-1

--- src/Components/PortfolioToolbar/PortfolioToolbar.tsx
+++ src/Components/PortfolioToolbar/PortfolioToolbar.tsx
@@ -1,10 +1,71 @@
-import {AppBar, Toolbar} from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  styled,
+  Button,
+  IconButton,
+} from '@mui/material';
+
+import {Menu} from '@mui/icons-material';
+
+const PageLink = styled(Button)(({theme}) => ({
+  marginRight: theme.spacing(4),
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
+  },
+}));
+
+const LastPageLink = styled(Button)(({theme}) => ({
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
+  },
+}));
+
+const HamburgerIconButton = styled(IconButton)(({theme}) => ({
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+  marginRight: theme.spacing(3),
+  marginLeft: 0,
+  paddingLeft: 0,
+}));
+
+const HamburgerMenu = () => {
+  return (
+    <>
+      <HamburgerIconButton>
+        <Menu />
+      </HamburgerIconButton>
+    </>
+  );
+};
 
 const PortfolioToolbar = () => {
   return (
-    <AppBar>
-      <Toolbar>Lee Dennis</Toolbar>
-    </AppBar>
+    <Box sx={{display: 'flex', justifyContent: 'space-evenly'}}>
+      <AppBar>
+        <Toolbar>
+          <HamburgerMenu />
+          <Typography variant="h6" sx={{flexGrow: 1}}>
+            Lee Dennis
+          </Typography>
+          <PageLink disableElevation variant="contained" href="#">
+            Home
+          </PageLink>
+          <PageLink disableElevation variant="contained" href="#">
+            Projects
+          </PageLink>
+          <PageLink disableElevation variant="contained" href="#">
+            Experience
+          </PageLink>
+          <LastPageLink disableElevation variant="contained" href="#">
+            Resume
+          </LastPageLink>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import {CssBaseline} from '@mui/material';
+import {ThemeProvider} from '@emotion/react';
+import {createTheme, CssBaseline} from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
 import './index.css';
 
+const theme = createTheme();
+
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
